eb550c7ec7cbae206fb4081441725dd248b8b7fa
Mattia Astorino <astorino.mattia@gmail.com>
2019-02-13 16:28:46 +0100

    chore: Improve demo.js

--- test/demo.js
+++ test/demo.js
@@ -1,7 +1,7 @@
 const elements = Array.from(document.querySelectorAll('[data-pura]'));
 
 const items = elements.map(item => {
-  item.addEventListener('click', function () {
+  item.addEventListener('click', () => {
     const playState = item.style.getPropertyValue('--pura-play-state');
     item.style.setProperty('--pura-play-state', !playState ? 'paused' : '');
   })
