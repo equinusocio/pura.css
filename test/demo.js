2b05430265c7ad6489298fcd72493ab984210910
Mattia Astorino <astorino.mattia@gmail.com>
2019-02-26 17:50:49 +0100

    chore: Add blur in

--- test/demo.js
+++ test/demo.js
@@ -1,8 +1,10 @@
-const elements = [...document.querySelectorAll('[data-pura]')];
+(function(){
+  const elements = [...document.querySelectorAll('[data-pura]')];
 
-const items = elements.map(item =>
-  item.addEventListener('click', () =>
-    item.style.setProperty('--pura-play-state', !!item.style.getPropertyValue('--pura-play-state')
-    ? ''
-    : 'paused'))
-);
+  const items = elements.map(item =>
+    item.addEventListener('click', () =>
+      item.style.setProperty('--pura-play-state', !!item.style.getPropertyValue('--pura-play-state') ?
+        '' :
+        'paused'))
+  );
+})()
